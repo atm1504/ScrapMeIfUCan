f9770448269a63874d60ef37a9a0d66fa35463a6
Amartya Mondal <hayyoulistentome@gmail.com>
2019-12-07 02:48:11 +0530

    feat: Scrap kaka's github

--- quotetutorial/quotetutorial/spiders/quotes_spider.py
+++ quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -2,8 +2,8 @@ import scrapy
 
 class QuoteSpider(scrapy.Spider):
     name="quotes"
-    start_urls = ["http://quotes.toscrape.com/"]
+    start_urls = ["https://github.com/somenath1435"]
 
     def parse(self, response):
-        title = response.css("title").extract()
+        title = response.css(".d-block.overflow-hidden::text").extract()
         yield {"titletext": title}
